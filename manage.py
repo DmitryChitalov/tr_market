cb0e0d8be2efa478d31b98a46ca559db4deee292
Dmitry Chitalov <35995629+DmitryChitalov@users.noreply.github.com>
2021-09-10 16:57:50 +0500

    обнова

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plaindjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market_prj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
